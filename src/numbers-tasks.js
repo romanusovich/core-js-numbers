ccdfbef53757584fb8874a380bd2541411bd4e80
roman <romanusovich1@gmail.com>
2026-09-18 16:31:19 +0200

    feat: implement getIntegerOnString

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -473,8 +473,9 @@ function getFloatOnString(str) {
  * '1.234', 2           => 1
  * '10', 8              => 8
  */
-function getIntegerOnString(/* str, base */) {
-  throw new Error('Not implemented');
+function getIntegerOnString(str, base) {
+  const parsed = Number.parseInt(str, base);
+  return Number.isNaN(parsed) ? NaN : parsed;
 }
 
 /**
