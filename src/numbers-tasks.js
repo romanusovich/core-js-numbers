d655438599b73c3b29b36c4eb7b7ff8fcddfe094
roman <romanusovich1@gmail.com>
2026-09-18 15:46:13 +0200

    feat: implement getFibonacciNumber

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -255,8 +255,20 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) return 0;
+  if (index === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+  let fib = 1;
+
+  for (let i = 2; i <= index; i += 1) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+  return fib;
 }
 
 /**
